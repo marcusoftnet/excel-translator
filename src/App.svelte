df6ba8e0b5b5977dde7121bf7241d5cc79c5d677
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-06-14 10:05:55 +0200

    Initial design

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,32 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<div class="jumbotron">
+  <h1 class="display-3">Excel formula translator</h1>
+  <p class="lead">
+    Paste your formula below and translate it into formulas for your langauge
+  </p>
+</div>
+<div class="jumbotron">
+  <div class="form-group">
+    <fieldset>
+      <label class="control-label" for="inputFormula">Input formula</label>
+      <input
+        class="form-control form-control-lg"
+        id="inputFormula"
+        type="text"
+        placeholder="=AVERAGE(B2:B34)" />
+      <label class="control-label" for="toLanguage">Language</label>
+      <select class="form-control form-control-lg" id="toLanguage">
+        <option>Swedish</option>
+      </select>
+    </fieldset>
+  </div>
+</div>
